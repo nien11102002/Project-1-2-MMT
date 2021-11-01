464a63909ad3078499459e3914a8f16d4ab077b6
Nguyen Manh Hung <nguyenmhung2310@gmail.com>
2021-11-01 16:27:24 +0700

    Edit lại header

--- Project-1-2-MMT/Game.h
+++ Project-1-2-MMT/Game.h
@@ -10,11 +10,16 @@
 #include<string>
 using namespace std;
 
+static int const quantity = 12;
+
 void ReadMap(ifstream& file, vector<vector<char>>& map1, vector<vector<char>>& map2);
-void ReadData1(ifstream& file, vector<vector<char>>& stat1);
-void ReadData2(ifstream& file, vector<vector<char>>& stat2);
+void ReadData(ifstream& file, vector<vector<char>>& stat);
 void PrintMap(vector<vector<char>>map);
 void PrintTableCloth(vector<vector<char>>stat);
 void Read(ifstream& file, vector<vector<char>>& stat1, vector<vector<char>>& stat2);
-
+int Random();
+void RunGame(vector<vector<char>>& map1, vector<vector<char>>& map2, vector<vector<char>>& stat1, vector<vector<char>>& stat2);
+int count(vector<vector<char>>enermystat);
+void Attack(vector<vector<char>>& map, vector<vector<char>>& allystat, vector<vector<char>>& enermystat, bool& flag);
+void DFS(vector<vector<char>>enermystat, int i, int j, int n, vector<vector<bool>>& visited);
 #endif
