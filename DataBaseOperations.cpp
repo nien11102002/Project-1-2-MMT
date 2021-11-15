804fe4ba6ecfce760997f84377870649270a3282
Nguyen Manh Hung <81510821+Hung369@users.noreply.github.com>
2021-11-15 09:12:17 +0700

    Little Update

--- DataBaseOperations.cpp
+++ DataBaseOperations.cpp
@@ -3,10 +3,11 @@
 	// Note từ Mạnh Hùng 13-11-2021.
 
 	fstream editor, jav;
+	unordered_map<Account*, Player*> hashmap;
+
 	editor.open("Acc and Pass.txt", ios::in);
 	jav.open("Player.txt", ios::in);
-
-	unordered_map<Account*, Player*> hashmap;
+		
 
 	if (!editor.is_open() && !jav.is_open()) {
 		cerr << "Can't open file.\n";
@@ -17,25 +18,25 @@
 	}
 	
 	jav.close(); editor.close();
-
 	string command;
+
 	do {
 		cout << ">> ";
 		getline(cin, command);
-
-		if (command == "log in") {
-
-		}
-		else if (command == "change password") {
-
-		}
+		
+		if (command == "log in")
+			Login(hashmap);
+		else if (command == "change password")
+			ChangePass(hashmap);
 		else if (command == "register") {
 
+		}
+		else if (command == "log out") {
+			cout << "Bye bye! Have a nice day.\n";
+			break;
 		}
 		else
 			cout << "Not match any results. Please enter again.\n" << endl;
 
-	} while (command != "quit" || command != "log out");
-
-	cout << "Bye bye, have a nice day.\n";
+	} while (true);
 }
