93e579d7a0b7011a8756a307fb88d39e07e369f6
Lulhoangminh <hoangminhbadao@gmail.com>
2021-11-18 14:10:03 +0700

    new function (not much)

--- Project-1-2-MMT/Login Fuctions.cpp
+++ Project-1-2-MMT/Login Fuctions.cpp
@@ -17,6 +17,19 @@ void Login(unordered_map<Account*, Player*> hashmap) {
 	// làm các hàm check user sau khi login tại đây.
 }
 
+void check_user_menu(){
+	cout << "Checking the status of another character: " << endl;
+	cout << "Choose one(1) of the option below:" << endl;
+	cout << "1. Find: -find" << endl;
+	cout << "2. Check online status: -online" << endl;
+	cout << "3. Show b-day: -show_date" << endl;
+	cout << "4. Show fullname: -show_fullname" << endl;
+	cout << "5. Show note: -show_note" << endl;
+	cout << "6. Show all information: -show_all" << endl;
+	cout << "7. Show point: -show_point" << endl
+	cout << endl << "Answer format should be: ";
+}
+
 bool isMatch(unordered_map<Account*, Player*> &hashmap, string account, string password, Player& user) {
 	for (auto it = hashmap.begin(); it != hashmap.end(); it++) 
 		if (it->first->Account_name() == account && it->first->Pass() == password) {
