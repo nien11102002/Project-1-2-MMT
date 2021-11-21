9d822eb46b5611269f15070c0d2a7ab04423d343
Nguyen Manh Hung <81510821+Hung369@users.noreply.github.com>
2021-11-21 11:08:08 +0700

    Small update in database operations

--- DataBaseOperations.cpp
+++ DataBaseOperations.cpp
@@ -18,7 +18,9 @@
 		else if (command == "change password")
 			ChangePass(hashmap);
 		else if (command == "register") {
-
+		    	cout << "> ";
+			getline(cin, command);
+			Register(hashmap, command);
 		}
 		else if (command == "log out") {
 			cout << "Bye bye! Have a nice day.\n";
