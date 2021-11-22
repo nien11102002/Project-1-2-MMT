efbba1096fb921d90201e81b2c070582828286c0
Lulhoangminh <hoangminhbadao@gmail.com>
2021-11-23 00:10:28 +0700

    hoàn chỉnh 2 hàm check_user và setup_info
    t check cũng kha khá r nhưng mà chưa test lỗi

--- Database/DataCore.h
+++ Database/DataCore.h
@@ -18,7 +18,7 @@ private:
 	long loss;
 	bool online;
 	string DOB;
-
+	string Note;
 public:
 	Player(string name, long vic, long los, string birthday) {
 		this->name = name;
@@ -33,12 +33,14 @@ public:
 	void setWin(long vic) { win = vic; }
 	void setLoss(long def) { loss = def; }
 	void setBirthday(string ser) { DOB = ser; }
+	void setNote(string note) { Note = note; }
 
 	string Name() { return name; }
 	long Win() { return win; }
 	long Loss() { return loss; }
 	bool Online() { return online;}
 	string Birthday() { return DOB; }
+	string getNote() { return Note; }
 
 	Player() {
 		name = "";
@@ -46,6 +48,7 @@ public:
 		win = 0;
 		loss = 0;
 		online = false;
+		Note = "Hello";
 	}
 
 	string ToString() {
@@ -53,7 +56,8 @@ public:
 		builder << name << "\n" << "Win: " << win << "\tLoss: " << loss << "\n";
 		builder << "Birthday: " << DOB << endl;
 		string onl = (online == true) ? "Online.\n" : "Offline.\n";
-		builder << "Status: " << onl;
+		builder << "Status: " << onl << endl;
+		builder << "Note: " << Note << endl;
 		string stream =  builder.str();
 		return stream;
 	}
--- Database/Login Fuctions.cpp
+++ Database/Login Fuctions.cpp
@@ -10,39 +10,48 @@ void Login(unordered_map<Account*, Player*> hashmap) {
 		cout << ">>Password: ";
 		getline(cin, password);
 	} while (isMatch(hashmap, account, password, user) != true);
-	
+	system("cls");
 	cout << "Welcome to BattleShip Online.\n";
 	cout<<user.ToString();
-	
 	// làm các hàm check user sau khi login tại đây.
 	cout << "What do you want to do?" << endl;
-	cout << "1. Checking the status of other players." << endl;
-	cout << "2. Setting up your personal information." << endl;
-	cout << endl << "0. Quit" << endl;
-	int choice;
+	cout << "1. Checking the status of other players: check_user [-option] [username]" << endl;
+	cout << "2. Setting up your personal information: setup_info [-option] [element]" << endl;
+	cout << "NOTE: If you need help, just type: \help" << endl;
+	string gura;
+	int flag = 0;
 	do{	
-		if(choice != 1 || choice != 2) 
-			cout << "You should only choose 1 or 2 ! Please re-enter";
+		if(get_gura(gura) == 0 && flag != 0) 
+			cout << "Unrecognizable choice! Please re-enter";
 		else cout << "Your choice: "; 
-		cin >> choice;
-	} while (choice != 1 || choice != 2);
+		cin >> gura;
+		flag++;
+	} while (get_gura(gura) == 0);
 
-	switch (choice)
+	switch (get_gura(gura))
 	{
 	case 1:
-		check_user_menu(hashmap);
+		check_user_menu(hashmap, gura);
 		break;
 	case 2:
-		setup_info_menu(hashmap, account);
+		setup_info_menu(hashmap, gura, account);
 		break;
-	case 0:
-		cout << "Goobye ! See you soon.";
+	case 3:
+		show_help_gura();
 	default:
 		break;
 	}
 }
 
-void check_user_menu(unordered_map<Account*, Player*>& hashmap){
+int get_gura(string input){
+	string gura_choice = input.substr(0, input.find_first_of(' '));
+	if(gura_choice == "check_user") return 1;
+	else if(gura_choice == "setup_info") return 2;
+	else if(gura_choice == "\help") return 3;
+	else return 0;
+}
+
+void show_help_gura(){
 	cout << "=== Checking the status of another player ===" << endl;
 	cout << "Choose one(1) of the option below:" << endl;
 	cout << "1. Find: -find" << endl;
@@ -52,35 +61,26 @@ void check_user_menu(unordered_map<Account*, Player*>& hashmap){
 	cout << "5. Show note: -show_note" << endl;
 	cout << "6. Show all information: -show_all" << endl;
 	cout << "7. Show point: -show_point" << endl;
-	cout << endl << "Answer format should be: check_user [-option] [username]" << endl;
-	cout << "Your choice:\n	\n\tcheck_user ";
-	string opt = "-null null";
-	do{
-		if(isValid_getline(opt))
-			getline(cin, opt);
-		else{
-		if(getoption(opt) == 0){
-			cout << "Error: invalid option! Please re-enter:";
-			getline(cin, opt);
-	}
-		else{
-			cout << "Wrong input format! Please re-enter:";
-			getline(cin, opt);
-	}	
-	}
-	} while(!isValid_getline(opt));
+	cout << endl;
+	cout << "=== Change your personal information ===" << endl;
+	cout << "Choose one(1) of the option below:" << endl;
+	cout << "1. Change your fullname: -fullname" << endl;
+	cout << "2. Change your date of birth: -date" << endl;
+	cout << "3. Change your note: -note" << endl;
+}
+void check_user_menu(unordered_map<Account*, Player*>& hashmap, string opt){	
 	int option = getoption(opt);
 	string username = getname(opt);
 	dispatch(hashmap, option, username);
 }
 
-bool isValid_getline(string opt){
-	bool result = 0;
-	regex input_pattern("-(\\w+_?\\w+)\\s(\\w+(\\s?\\w+)*)");
-	if(regex_match(opt,input_pattern))
-	result = 1;
-	return result;
-}
+// bool isValid_getline(string opt){
+// 	bool result = 0;
+// 	regex input_pattern("-(\\w+_?\\w+)\\s(\\w+(\\s?\\w+)*)");
+// 	if(regex_match(opt,input_pattern))
+// 	result = 1;
+// 	return result;
+// }
  
 int getoption(string opt){
 	const string opt1 = "find";
@@ -185,7 +185,7 @@ void show_Fullname(unordered_map<Account*, Player*> &hashmap, string username){
 void show_Note(unordered_map<Account*, Player*> &hashmap, string username){
 	for (auto it = hashmap.begin(); it != hashmap.end(); it++) 
 		if (it->first->Account_name() == username)
-			cout << "Player " << username << "has no note";
+			cout << "Player " << username << "'s note: " << it->second->getNote();
 	if(!find_Name(hashmap, username))
 		cout << "Player " << username << " does not exist!";	
 }
@@ -212,38 +212,19 @@ void show_Point(unordered_map<Account*, Player*> &hashmap, string username){
 		cout << "Player " << username << " does not exist!";
 }
 
-void setup_info_menu(unordered_map<Account*, Player*>& hashmap, string username){
-	cout << "=== Change your personal information ===" << endl;
-	cout << "Choose one(1) of the option below:" << endl;
-	cout << "1. Change your fullname: -fullname" << endl;
-	cout << "2. Change your date of birth: -date" << endl;
-	cout << "3. Change your note: -note" << endl;
-	cout << endl << "Answer format should be: setup_info [-option] [element]\nNote: [element] should be according to the option that you choose" << endl;
-	cout << "Your choice:\n\n\tsetup_info ";
-	string opt = "-NUll";
-	do{
-		if(isValid_getline(opt))
-			getline(cin, opt);
-		else{
-		if(getoption_setup(opt) == 0)
-			cout << "Error: invalid option! Please re-enter:";
-		else cout << "Wrong input format! Please re-enter:";
-			getline(cin, opt);
-		} 
-	}
-	while(!isValid_getline(opt));
-	int option = getoption_setup(opt);
-	string element = getelement_setup(opt);
+void setup_info_menu(unordered_map<Account*, Player*>& hashmap, string opt_setup, string username){
+	int option = getoption_setup(opt_setup);
+	string element = getelement_setup(opt_setup);
 	dispatch_setup(hashmap, option, element, username);
 }
 
-bool isValid_getline_setup(string opt){
-	bool result = 0;
-	regex option_pattern("-(\\w+)");
-	if(regex_match(opt,option_pattern))
-	result = 1;
-	return result;
-}
+// bool isValid_getline_setup(string opt){
+// 	bool result = 0;
+// 	regex option_pattern("-(\\w+)");
+// 	if(regex_match(opt,option_pattern))
+// 	result = 1;
+// 	return result;
+// }
 
 int getoption_setup(string opt){
 	const string opt1 = "fullname";
@@ -276,13 +257,13 @@ void dispatch_setup(unordered_map<Account*, Player*> &hashmap, int option, strin
 	switch (option)
 	{
 	case change_fullname:
-		
+		change_Fullname(hashmap, option, element, username);
 		break;
 	case change_dob:
-
+		change_Dob(hashmap, option, element, username);
 		break;
 	case change_note:
-	
+		change_Note(hashmap, option, element, username);
 		break;
 	default:
 		break;
@@ -291,12 +272,27 @@ void dispatch_setup(unordered_map<Account*, Player*> &hashmap, int option, strin
 
 void change_Fullname(unordered_map<Account*, Player*> &hashmap, int option, string element, string username){
 	for (auto it = hashmap.begin(); it != hashmap.end(); it++) 
-		if (it->first->Account_name() == username)
-			cout << "Player " << username << "'s fullname is: " << it->second->Name();
-	if(!find_Name(hashmap, username))
-		cout << "Player " << username << " does not exist!";	
+		if (it->first->Account_name() == username){
+			it->second->setName(element);
+			cout << "Name of player " << username << " has been changed to" << it->second->Name();
+		}	
 }
 
+void change_Dob(unordered_map<Account*, Player*> &hashmap, int option, string element, string username){
+	for (auto it = hashmap.begin(); it != hashmap.end(); it++) 
+		if (it->first->Account_name() == username){
+			it->second->setBirthday(element);
+			cout << "Birthday of player " << username << " has been changed to" << it->second->Name();
+		}
+}
+
+void change_Note(unordered_map<Account*, Player*> &hashmap, int option, string element, string username){
+	for (auto it = hashmap.begin(); it != hashmap.end(); it++) 
+		if (it->first->Account_name() == username){
+			it->second->setBirthday(element);
+			cout << "Birthday of player " << username << " has been changed to" << it->second->Name();
+		}
+}
 bool isMatch(unordered_map<Account*, Player*> &hashmap, string account, string password, Player& user) {
 	for (auto it = hashmap.begin(); it != hashmap.end(); it++) 
 		if (it->first->Account_name() == account && it->first->Pass() == password) {
@@ -305,7 +301,6 @@ bool isMatch(unordered_map<Account*, Player*> &hashmap, string account, string p
 			user.setOnline(true);
 			user.setWin(it->second->Win());
 			user.setLoss(it->second->Loss());
-
 			it->second->setOnline(true);
 			return true;
 		}
