b9aebf40371cbb88d805dd3fad79a6275de88c28
Nguyen Manh Hung <81510821+Hung369@users.noreply.github.com>
2021-11-27 09:49:16 +0700

    Hoàn thành xong phần Client 100%, chuẩn bị tiến đánh Server

--- Client.cpp
+++ Client.cpp
@@ -1,69 +1,34 @@
-#include"Client Header.h"
+#include"MyClient.h"
 
 int main() {
-	WSAData data;
-	WORD ver = MAKEWORD(2, 2);
-	int wsResult = WSAStartup(ver, &data);
-	if (wsResult != 0){
-		cerr << "Can't start Winsock, Err #" << wsResult << endl;
-		return 0;
-	}
+	Client hexgate;
+	string msg, username;
 
-	// Create a socket.
-	SOCKET hexgate = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (hexgate == INVALID_SOCKET){// check if it failed to create.
-		cerr << "Can't create socket, Err #" << WSAGetLastError() << endl;
-		WSACleanup();
-		return 0;
-	}
+	cout << "Enter your username." << endl;
+	getline(cin, username);
 
-	//Complete the core of hexgate 
-	sockaddr_in core;
-	core.sin_family = AF_INET;
-	core.sin_port = htons(stoi(port));
-	inet_pton(AF_INET, IP.c_str(), &core.sin_addr);
+	if (hexgate.initSocket()) {
 
-	// Connect to Server
-	if (connect(hexgate, (SOCKADDR*)&core, sizeof(core)) == 0) {
-		cout << "Connected!" << endl;
-		string mess;
+		hexgate.ConnectSocket();
+
+		hexgate.recvThread = thread([&] { hexgate.ThreadRecv(); });
 
 		while (true) {
-			cout << "> ";
-			getline(cin, mess);
-
-			if (mess.compare("quit")){
-				cout << "Break the connection.\n";
-				break;
+			getline(cin, msg);
+			string messageToSend;
+			if (hexgate.joinChat == false) {
+				std::ostringstream ss;
+				ss << username << ": " << msg;
+				messageToSend = ss.str();
 			}
-
-			//message before send
-			int iResult = send(hexgate, mess.c_str(), strlen(mess.c_str()), 0);
-
-			if (iResult <= 0)
-			{
-				cout << "send() failed: " << WSAGetLastError() << endl;
-				break;
+			else if (hexgate.joinChat == true) {
+				std::ostringstream ss;
+				ss << username << " joined the chat!";
+				messageToSend = ss.str();
+				hexgate.joinChat = false;
 			}
-			
+			hexgate.Sending(messageToSend);
 		}
 	}
-	else {
-		cout << "Error Connecting to Host" << endl;
-		exit(1);
-	}
-
-	//close socket.
-	int result = shutdown(hexgate, SD_SEND);
-	if (result == SOCKET_ERROR) {
-		cout << "shutdown() failed with error: " << WSAGetLastError() << endl;
-		closesocket(hexgate);
-		WSACleanup();
-		return 1;
-	}
-
-	closesocket(hexgate);
-	WSACleanup();
-
 	return 0;
 }
