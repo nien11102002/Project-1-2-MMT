12e4e54d9876425a973097006958802e203d5474
Nguyen Manh Hung <81510821+Hung369@users.noreply.github.com>
2021-11-27 20:27:55 +0700

    khởi tạo hàm trống cho build class server

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,29 +1,7 @@
 #include"Server Header.h"
 
 int main() {
-	WSAData data;
-	WORD ver = MAKEWORD(2, 2);
-	int wsResult = WSAStartup(ver, &data);
-	if (wsResult != 0) {
-		cerr << "Can't start Winsock, Err #" << wsResult << endl;
-		return 0;
-	}
-
-	// Create a socket
-	SOCKET hexcore = socket(AF_INET, SOCK_STREAM, 0);
-	if (hexcore == INVALID_SOCKET)
-	{
-		cerr << "Can't create a socket! Exit program" << endl;
-		return 0;
-	}
-
-	// Bind the ip address and port to a socket
-	sockaddr_in hexa;
-	hexa.sin_family = AF_INET;
-	hexa.sin_port = htons(stoi(port));
-	hexa.sin_addr.S_un.S_addr = INADDR_ANY;
-
-	bind(hexcore, (sockaddr*)&hexa, sizeof(hexa));
+	
 
 	return 0;
 }
