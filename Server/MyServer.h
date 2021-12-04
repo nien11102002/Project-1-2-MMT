7c8f0aeef2dbf14a4a613198cae6931c2d3e39f3
Nguyen Manh Hung <81510821+Hung369@users.noreply.github.com>
2021-12-04 16:18:56 +0700

    Xong phần Server

--- Server/MyServer.h
+++ Server/MyServer.h
@@ -1,9 +1,10 @@
-#ifndef MYSERVER_H
+﻿#ifndef MYSERVER_H
 #define MYSERVER_H
 
+#define WIN32_LEAN_AND_MEAN
 #include<iostream>
 #include<string>
-#include<thread>
+#include<vector>
 #include<sstream>
 #include<WinSock2.h>
 #include<WS2tcpip.h>
@@ -11,13 +12,18 @@
 #pragma comment (lib,"WS2_32.lib")
 using namespace std;
 
-const int MAX_BUFFER_SIZE = 4096;
+struct client_table {
+	bool connected;
+	SOCKET client_gate;
+};
 
 class Server {
 private:
 	SOCKET clientSocket;// socket de nhan va ket noi voi client.
+	sockaddr_in hint;
 	string listenIP;
 	int listenPortNum;
+	char buffer[1024] = { '\0' };
 
 public:
 	Server(string IP, int portnumber) {
@@ -38,19 +44,27 @@ public:
 
 	void CreateSocket() {
 
+		int i = 1;
 		clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
 		if (clientSocket != INVALID_SOCKET) {
 
-			sockaddr_in hint;
+			setsockopt(clientSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&i, sizeof(i));
+
+			
 			hint.sin_family = AF_INET;
 			hint.sin_port = htons(listenPortNum);
 			inet_pton(AF_INET, listenIP.c_str(), &hint.sin_addr);
+
+
 			int bindCheck = bind(clientSocket, (sockaddr*)&hint, sizeof(hint));
 
 			if (bindCheck != SOCKET_ERROR) {//If bind OK:
 
 				int listenCheck = listen(clientSocket, SOMAXCONN);	//Tell the socket is for listening. 
+				unsigned long b = 1;
+				ioctlsocket(clientSocket, FIONBIO, &b);
+
 				if (listenCheck == SOCKET_ERROR) {
 					cerr << "Can't listen from client.\n";
 					closesocket(clientSocket);
@@ -68,14 +82,84 @@ public:
 		}
 		else {
 			cout << "Error: can't create socket in server." << endl;
+			closesocket(clientSocket);
 			WSACleanup();
 			exit(-1);
 		}
 
 	}
 
-	void Send() {// hàm gửi đến một client cụ thể, hàm chưa hoàn chỉnh
+	void Run() {
+		CreateSocket();
+		int connected_clients = 0;
+		vector<client_table> socialcredit;
 
+		while (true) {
+			int len = sizeof(hint);
+			SOCKET connectionline = accept(clientSocket, (sockaddr*)&hint, &len);
+
+			if (connectionline != INVALID_SOCKET) 
+			{
+				client_table temp;
+
+				temp.client_gate = connectionline;
+				temp.connected = true;
+				connected_clients++;
+
+				socialcredit.push_back(temp);
+				cout << "New client: " << connectionline << endl;
+				cout << "Online client(s): " << connected_clients << endl;
+			}
+
+			Sleep(1);
+
+			if (connected_clients > 0) {// there are connected clients available.
+
+				for (int cs = 0; cs < socialcredit.size(); cs++) {
+
+					ZeroMemory(buffer, 1024);// reset buffer.
+
+					if (socialcredit[cs].connected == true) {
+
+						int receivers = recv(socialcredit[cs].client_gate, buffer, 1024, 0);
+
+						// closing a client connection.
+						if (Exit(buffer)) {
+							cout << "An ally has disconnected.\n";
+							socialcredit[cs].connected = false;
+							auto iter = socialcredit.begin() + cs;
+							socialcredit.erase(iter);
+							connected_clients--;
+						}
+						else if (receivers > 0) 
+						{
+							cout << "Client data received: " << buffer << endl;
+								//sending to others client in a chat server
+							Sleep(10);
+							
+							int rep = send(socialcredit[cs].client_gate, buffer, receivers + 1, 0);// gui k đc
+							if (rep == SOCKET_ERROR)
+							{
+								cout << "LOI\n";
+							}
+														
+						}
+							
+
+					}
+
+				}
+
+			}
+		}
+	}
+
+	bool Exit(char buffer[]) {
+		string msg = string(buffer);
+		if (msg=="disconnected") {
+			return true;
+		}
+		return false;
 	}
 
 	~Server() {
--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,7 +1,9 @@
-#include"Server Header.h"
+#include"MyServer.h"
 
 int main() {
-	
-
+	Server myServer("127.0.0.1",54010);
+	if (myServer.initSocket() == true) {
+		myServer.Run();
+	}
 	return 0;
 }
