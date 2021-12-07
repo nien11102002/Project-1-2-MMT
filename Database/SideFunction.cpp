1470a25c30f3be0906032f06f90ab79f061e7e48
nien11102002 <nguyenduynien11102002@gmail.com>
2021-12-07 20:16:29 +0700

    Update SideFunction.cpp

--- Database/SideFunction.cpp
+++ Database/SideFunction.cpp
@@ -1,6 +1,6 @@
 #include "DataCore.h"
 
-void PrintOnlinePlayers(unordered_map<Account*, Player*> hashmap, unordered_map<Account*, Player*>& store)
+void PrintOnlinePlayers(unordered_map<Account*, Player*> hashmap)
 {
 	cout << "List users are online: ";
 	for (auto i = hashmap.begin(); i != hashmap.end(); i++)
