80c49083ad6590076de833116edcd14381553f77
nien11102002 <nguyenduynien11102002@gmail.com>
2021-12-18 01:53:19 +0700

    Đã tới code chạy game nhưng chưa hoàn tất, 50%

--- Server/MyServer.h
+++ Server/MyServer.h
@@ -1,4 +1,4 @@
-﻿#ifndef MYSERVER_H
+#ifndef MYSERVER_H
 #define MYSERVER_H
 
 #define WIN32_LEAN_AND_MEAN
@@ -130,7 +130,6 @@ public:
 
 					ZeroMemory(buffer, 1024);// reset buffer.
 
-
 					if (socialcredit[cs].connected == true) {
 
 						int receivers = recv(socialcredit[cs].client_gate, buffer, 1024, 0);
@@ -146,21 +145,30 @@ public:
 						}
 						else if (receivers > 0)
 						{
-							cout << "Client "<<socialcredit[cs].client_gate << " data received : " << buffer << endl;
+							cout << "Client " << socialcredit[cs].client_gate << " data received : " << buffer << endl;
 							if (!socialcredit[cs].logged) {
 								string m = string(buffer);
 								auto first = m.find_first_of(' ');
 
-								string type = m.substr(0, first);
-								string content = m.substr(first + 1, m.size() - first - 1);
+								string type, content;
+								if (first != -1)
+								{
+									type = m.substr(0, first);
+									content = m.substr(first + 1, m.size() - first - 1);
+								}
+								else type = m;
+
 								if (type == "login" || type == "Login") {
 									bool flag = false;
-									
-									Login(hashmap, socialcredit[cs],flag, content);
+									Login(hashmap, socialcredit[cs], flag, content);
 									socialcredit[cs].logged = flag;
 								}
 								else if (type == "register" || type == "Register") {
 									Register(hashmap, socialcredit[cs], content);
+								}
+								else if (type == "change_password" || type == "Change_password")
+								{
+									ChangePassword(hashmap, socialcredit[cs]);
 								}
 								else {
 									string warning = "not match any type.\n";
@@ -170,15 +178,7 @@ public:
 								}
 							}
 							else {
-								string mess = string(buffer);
-								int first_space = mess.find_first_of(' ');
-								string option = mess.substr(0, first_space);
-								if (option == "check_user") {
-									check_user_menu(hashmap, mess);
-								}
-								else if (option == "setup_info"){
-									setup_info_menu(hashmap, mess, );
-								}
+
 							}
 						}
 
@@ -190,20 +190,6 @@ public:
 		}
 	}
 
-	int get_option(string input) {
-		const regex quit_pattern("[Qq][Uu][Ii][Tt]");
-		if (regex_match(input, quit_pattern)) return -1;
-		string gura_choice = input.substr(0, input.find_first_of(' '));
-		if (gura_choice == "check_user") return 1;
-		else if (gura_choice == "setup_info") return 2;
-		else if (gura_choice == "/help") {
-			string function_string = input.substr(input.find_first_of(' ') + 1, input.size() - (input.find_first_of(' ') + 1));
-			if (function_string == "check_user") return 3;
-			else if (function_string == "setup_info") return 4;
-		}
-		return 0;
-	}
-
 	bool Exit(char buffer[]) {
 		string msg = string(buffer);
 		if (msg == "Logout" || msg == "logout") {
@@ -212,7 +198,7 @@ public:
 		return false;
 	}
 
-	void Login(unordered_map<Account*, Player*> hashmap, client_table& the_wok, bool& flag, string content, string& user)
+	void Login(unordered_map<Account*, Player*> hashmap, client_table& the_wok, bool& flag, string content)
 	{
 		the_wok.account = content;
 
@@ -226,14 +212,14 @@ public:
 
 		the_wok.pass = var;
 
-		if (isMatch(hashmap, the_wok.account, the_wok.pass, the_wok.citizen)) 
+		if (isMatch(hashmap, the_wok.account, the_wok.pass, the_wok.citizen))
 		{
 			sendmsg = "\nLogin successfully!\n";
 			Sleep(500);
 			SendTo(the_wok.client_gate, sendmsg);
 			flag = true;
 		}
-		else 
+		else
 		{
 			sendmsg = "Error, do not match any username.";
 			Sleep(500);
@@ -242,7 +228,7 @@ public:
 
 	}
 
-	void Register(unordered_map<Account*, Player*>& hashmap, client_table& the_wok, string content) 
+	void Register(unordered_map<Account*, Player*>& hashmap, client_table& the_wok, string content)
 	{
 		string sendmsg, var;
 		if (isAvailableUsername(hashmap, content) == true)
@@ -266,7 +252,7 @@ public:
 		the_wok.pass = var;
 
 		sendmsg = "Do you want to encrypt your message before sending?";
-		SendTo(the_wok.client_gate, sendmsg); 
+		SendTo(the_wok.client_gate, sendmsg);
 		var = ReceiveFrom(the_wok.client_gate);
 		if (var == "Error in recv msg") {
 			cout << var << endl;
@@ -274,13 +260,13 @@ public:
 		}
 
 		int encrypted = 0;
-		if (var == "Y") 
-		{ 
+		if (var == "Y")
+		{
 			encrypted = 1; sendmsg = "Register successfully and Message was encrypted.";
 			Sleep(100);
 			SendTo(the_wok.client_gate, sendmsg);
 		}
-		else 
+		else
 		{
 			encrypted = 0; sendmsg = "Register successfully and Message was not encrypted.";
 			Sleep(100);
@@ -312,12 +298,219 @@ public:
 		Account A(the_wok.account, encrypted, the_wok.pass);
 		writeAtBottomOfNewOne(C, A);
 
-		hashmap.insert(make_pair(new Account(the_wok.account, encrypted, the_wok.pass), 
-			new Player(C.Name(),0,0,C.Birthday())));
+		hashmap.insert(make_pair(new Account(the_wok.account, encrypted, the_wok.pass),
+			new Player(C.Name(), 0, 0, C.Birthday())));
 
 		sendmsg = "Registered successfully.\n"; Sleep(500);
 		SendTo(the_wok.client_gate, sendmsg);
 	}
+
+	void ChangePassword(unordered_map<Account*, Player*> hashmap, client_table& the_wok)
+	{
+		string sendmsg = "password: ";
+		SendTo(the_wok.client_gate, sendmsg);
+		string var;
+		do
+		{
+			var = ReceiveFrom(the_wok.client_gate);
+			if (var == "Error in recv msg") {
+				cout << var << endl;
+				return;
+			}
+
+			if (var != the_wok.pass)
+			{
+				string rep = "password: ";
+				SendTo(the_wok.client_gate, rep);
+			}
+		} while (var != the_wok.pass);
+		sendmsg = "Do you want to encrypt your message before sending?";
+		SendTo(the_wok.client_gate, sendmsg);
+
+		var = ReceiveFrom(the_wok.client_gate);
+		if (var == "Error in recv msg") {
+			cout << var << endl;
+			return;
+		}
+		string flag = var;
+
+		sendmsg = "new password: ";
+		SendTo(the_wok.client_gate, sendmsg);
+		
+		var = ReceiveFrom(the_wok.client_gate);
+		if (var == "Error in recv msg") {
+			cout << var << endl;
+			return;
+		}
+
+		the_wok.pass = var;
+
+		if (flag == "Y")
+			sendmsg = "Changepassword successfully and Message was encrypted.";
+		else sendmsg = "Changepassword successfully and Message wasn't encrypted.";
+		SendTo(the_wok.client_gate, sendmsg);
+
+		for (auto it = hashmap.begin(); it != hashmap.end(); it++)
+		{
+			if (it->first->Account_name() == the_wok.account)
+			{
+				if (flag == "Y")
+					it->first->setPassword(the_wok.pass, 1);
+				else it->first->setPassword(the_wok.pass, 0);
+				break;
+			}
+		}
+	}
+
+	
+	void Server_CreateRoom(unordered_map<Account*, Player*> hashmap, client_table the_wok, vector<client_table> socialcredit,
+		string message)
+	{
+		CheckValidRoomID(hashmap, the_wok, socialcredit, message);
+		string var;
+		int SpaceIndex = message.find_last_of(" ");
+		string OpponentName = message.substr(SpaceIndex + 1, message.length() - SpaceIndex - 1);
+		int isOnline = 0;
+		client_table Opponent;
+		FindUser(OpponentName, socialcredit, isOnline, Opponent);
+		stringstream builder;
+		if (isOnline == 1)//1 la online, 0 la offline, 2 la không tồn tại username
+		{
+			builder << "Invitation User " + the_wok.account + " has sent you an invitation to a match.\n";
+			builder << "";
+			string reply = builder.str();
+
+			SendTo(Opponent.client_gate, reply);
+			var = ReceiveFrom(Opponent.client_gate);
+			if (var == "Error in recv msg") {
+				cout << var << endl;
+				return;
+			}
+
+			message = var;
+			do {
+				if (message == "Y")
+				{
+					string reply = "Game start";
+					SendTo(the_wok.client_gate, reply);
+
+					SendTo(Opponent.client_gate, reply);
+				}
+				else if (message == "N")
+				{
+					string reply = "Opponent refuse to play.";
+					SendTo(the_wok.client_gate, reply);
+
+					string reply = "Has sent your refuse.";
+					SendTo(Opponent.client_gate, reply);
+				}
+				else
+				{
+					string reply = "Invitation Invalid answer!!! Please correctly resend!!!";
+					SendTo(Opponent.client_gate, reply);
+				}
+			} while (message != "Y" || message != "N");
+		}
+		else if (isOnline == 0)
+		{
+			string reply = "Username is offline.";
+			SendTo(the_wok.client_gate, reply);
+		}
+		else
+		{
+			string reply = "Wrong username.";
+			SendTo(the_wok.client_gate, reply);
+		}
+	}
+
+	void FindUser(string name, vector<client_table> socialcredit, int& isOnline, client_table& c)
+	{
+		for (auto i = 0; i < socialcredit.size(); i++)
+		{
+			if (socialcredit[i].account == name)
+			{
+				c = socialcredit[i];
+				isOnline = c.logged;
+				return;
+			}
+		}
+		isOnline = 2;
+	}
+
+	void CheckValidRoomID(unordered_map<Account*, Player*> hashmap, client_table the_wok, vector<client_table> socialcredit,
+		string message)
+	{
+		string tmp = message.substr(12, message.length() - 12);
+		int SpaceIndex = tmp.find_first_of(" ");
+		int a = stoi("1" + tmp.substr(0, SpaceIndex));
+		int index = log10(a) + 1;
+		int RoomID = a / pow(10, index - 1);
+		if (log10(RoomID) + 1 != tmp.substr(0, SpaceIndex).length() || RoomID != 0)
+		{
+			string reply = "Invalid Room ID.";
+
+			int rep = send(the_wok.client_gate, reply.c_str(), reply.size(), 0);
+			if (rep == SOCKET_ERROR)
+				cout << "Error to send.\n";
+		}
+	}
+
+	void StartGame(client_table the_wok,client_table opponent)
+	{
+		string var1, var2;
+		bool flag = 1;
+		do
+		{
+			var1 = ReceiveFrom(the_wok.client_gate);
+			var2 = ReceiveFrom(opponent.client_gate);
+
+			string tmp1 = var1.substr(0, var1.find(" "));
+			string tmp2 = var2.substr(0, var2.find(" "));
+			if (tmp1 == "upload_file" && tmp2 == "upload_file")
+			{
+				string rep = "Game begin";
+				SendTo(the_wok.client_gate, rep);
+				SendTo(opponent.client_gate, rep);
+				flag = 0;
+			}
+			else
+			{
+				if (tmp1 != "upload_file")
+				{
+					string rep = "Wrong format";
+					SendTo(the_wok.client_gate, rep);
+				}
+				if (tmp2 != "upload_file")
+				{
+					string rep = "Wrong format";
+					SendTo(opponent.client_gate, rep);
+				}
+			}
+		} while (flag);
+	}
+
+	void GameHandle(bool& game_over, client_table the_wok, client_table opponent, string cmd)
+	{
+		do
+		{
+			SendTo(opponent.client_gate, cmd);
+			string var = ReceiveFrom(opponent.client_gate);//receive "hit" or "miss" or "game over"
+			
+			game_over = isGameOver(var);
+			SendTo(the_wok.client_gate, var);
+			if (var == "hit")
+			{
+				var = ReceiveFrom(the_wok.client_gate);
+				continue;
+			}
+			else if (var == "miss")
+			{
+				var = ReceiveFrom(opponent.client_gate);
+			}
+		} while (!game_over);
+	}
+
+
 
 
 	string ReceiveFrom(SOCKET sock_id) {
@@ -341,222 +534,4 @@ public:
 	}
 
 };
-
-void check_user_menu(unordered_map<Account*, Player*>& hashmap, string opt) {
-	int option = getoption(opt);
-	string username = getname(opt);
-	dispatch(hashmap, option, username);
-}
-
-int getoption(string opt) {
-	const string opt1 = "find";
-	const string opt2 = "online";
-	const string opt3 = "show_date";
-	const string opt4 = "show_fullname";
-	const string opt5 = "show_note";
-	const string opt6 = "show_all";
-	const string opt7 = "show_point";
-	regex option_pattern("-(\\w+_?\\w+)");
-	smatch match;
-	string option_string;
-	regex_search(opt, match, option_pattern);
-	option_string = match[1];
-	if (option_string == opt1) return 1;
-	else if (option_string == opt2) return 2;
-	else if (option_string == opt3) return 3;
-	else if (option_string == opt4) return 4;
-	else if (option_string == opt5) return 5;
-	else if (option_string == opt6) return 6;
-	else if (option_string == opt7) return 7;
-	else return 0;
-}
-
-string getname(string opt) {
-	string user_string;
-	smatch match;
-	regex user_pattern("\\s(\\w+(\\s?\\w+)*)");
-	regex_search(opt, match, user_pattern);
-	user_string = match[1];
-	return user_string;
-}
-
-
-void dispatch(unordered_map<Account*, Player*>& hashmap, int option, string username) {
-	const int find_name = 1;
-	const int check_online = 2;
-	const int show_dob = 3;
-	const int show_fullname = 4;
-	const int show_note = 5;
-	const int show_all = 6;
-	const int show_point = 7;
-	switch (option)
-	{
-	case find_name:
-		if (find_Name(hashmap, username)) cout << "Player " << username << " exists!\n\n";
-		else cout << "Player " << username << " does not exist!\n\n";
-		break;
-	case check_online:
-		if (check_Online(hashmap, username)) cout << "Player " << username << " is online\n\n";
-		else if (!find_Name(hashmap, username)) cout << "Player " << username << " does not exist!\n\n";
-		else cout << "Player " << username << " is not online!\n\n";
-		break;
-	case show_dob:
-		show_DOB(hashmap, username);
-		break;
-	case show_fullname:
-		show_Fullname(hashmap, username);
-		break;
-	case show_note:
-		show_Note(hashmap, username);
-		break;
-	case show_all:
-		show_All(hashmap, username);
-		break;
-	case show_point:
-		show_Point(hashmap, username);
-		break;
-	default:
-		break;
-	}
-}
-
-
-bool find_Name(unordered_map<Account*, Player*>& hashmap, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->first->Account_name() == username) return true;
-	return false;
-}
-
-bool check_Online(unordered_map<Account*, Player*>& hashmap, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->first->Account_name() == username)
-			if (it->second->Online()) return true;
-	return false;
-}
-
-void show_DOB(unordered_map<Account*, Player*>& hashmap, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->first->Account_name() == username)
-			cout << "Player " << username << "'s date of birth is: " << it->second->Birthday() << endl << endl;
-	if (!find_Name(hashmap, username))
-		cout << "Player " << username << " does not exist!\n\n";
-}
-
-void show_Fullname(unordered_map<Account*, Player*>& hashmap, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->first->Account_name() == username)
-			cout << "Player " << username << "'s fullname is: " << it->second->Name() << endl << endl;
-	if (!find_Name(hashmap, username))
-		cout << "Player " << username << " does not exist!\n\n";
-}
-
-void show_Note(unordered_map<Account*, Player*>& hashmap, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->first->Account_name() == username)
-			cout << "Player " << username << "'s note: " << it->second->getNote() << endl << endl;
-	if (!find_Name(hashmap, username))
-		cout << "Player " << username << " does not exist!\n\n";
-}
-
-void show_All(unordered_map<Account*, Player*>& hashmap, string username) {
-	cout << "All information about Player: " << username << endl;
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->first->Account_name() == username) {
-			cout << "Fullname: " << it->second->Name() << endl;
-			cout << "Status: " << it->second->Online() << endl;
-			cout << "Date of Birth: " << it->second->Birthday() << endl;
-			cout << "Win: " << it->second->Win() << endl;
-			cout << "Loss: " << it->second->Loss() << endl << endl;
-		}
-	if (!find_Name(hashmap, username))
-		cout << "Player " << username << " does not exist!\n\n";
-}
-
-void show_Point(unordered_map<Account*, Player*>& hashmap, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->first->Account_name() == username)
-			cout << "Player " << username << "'s point:\n Victory: " << it->second->Win()
-			<< "\nDefeat: " << it->second->Loss() << endl << endl;
-	if (!find_Name(hashmap, username))
-		cout << "Player " << username << " does not exist!\n\n";
-}
-
-void setup_info_menu(unordered_map<Account*, Player*>& hashmap, string opt_setup, string username) {
-	int option = getoption_setup(opt_setup);
-	string element = getelement_setup(opt_setup);
-	dispatch_setup(hashmap, option, element, username);
-}
-
-
-
-int getoption_setup(string opt) {
-	const string opt1 = "fullname";
-	const string opt2 = "date";
-	const string opt3 = "note";
-	regex option_pattern("-(\\w+)");
-	smatch match;
-	string option_string;
-	regex_search(opt, match, option_pattern);
-	option_string = match[1];
-	if (option_string == opt1) return 1;
-	else if (option_string == opt2) return 2;
-	else if (option_string == opt3) return 3;
-	else return 0;
-}
-
-
-string getelement_setup(string opt) {
-	string element_string;
-	smatch match;
-	regex user_pattern("\\s(\\w+(\\s?\\w+)*)");
-	regex_search(opt, match, user_pattern);
-	element_string = match[1];
-	return element_string;
-}
-
-
-void dispatch_setup(unordered_map<Account*, Player*>& hashmap, int option, string element, string username) {
-	const int change_fullname = 1;
-	const int change_dob = 2;
-	const int change_note = 3;
-	switch (option)
-	{
-	case change_fullname:
-		change_Fullname(hashmap, option, element, username);
-		break;
-	case change_dob:
-		change_Dob(hashmap, option, element, username);
-		break;
-	case change_note:
-		change_Note(hashmap, option, element, username);
-		break;
-	default:
-		break;
-	}
-}
-
-
-void change_Fullname(unordered_map<Account*, Player*>& hashmap, int option, string element, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->second->Name() == username) {
-			it->second->setName(element);
-			cout << "Name of player " << username << " has been changed to" << it->second->Name() << endl << endl;
-		}
-}
-
-void change_Dob(unordered_map<Account*, Player*>& hashmap, int option, string element, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->second->Name() == username) {
-			it->second->setBirthday(element);
-			cout << "Birthday of player " << username << " has been changed to" << it->second->Name() << endl << endl;
-		}
-}
-
-void change_Note(unordered_map<Account*, Player*>& hashmap, int option, string element, string username) {
-	for (auto it = hashmap.begin(); it != hashmap.end(); it++)
-		if (it->second->Name() == username) {
-			it->second->setBirthday(element);
-			cout << "Birthday of player " << username << " has been changed to" << it->second->Name() << endl << endl;
-		}
-}
 #endif
