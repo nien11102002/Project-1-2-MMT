9c8403e4d2dbc0d7af46e210f9fbec5b9c56b3ef
nien11102002 <nguyenduynien11102002@gmail.com>
2021-12-18 02:06:11 +0700

    Đang làm phần game (Còn desription)
    Phần này chưa gộp phần của Minh. Đang ms làm thẳng phần game trong main cho đỡ rối

--- Client/ClientMain.cpp
+++ Client/ClientMain.cpp
@@ -1,4 +1,5 @@
-﻿#include"MyClient.h"
+#include"MyClient.h"
+#include "Game.h"
 
 int main() {
 	Client hexgate;
@@ -12,80 +13,93 @@ int main() {
 
 		while (true)
 		{
-			regex input_login("[Ll][Oo][Gg][Ii][Nn]");
-			regex input_logout("[Ll][Oo][Gg][Oo][Uu][Tt]");
-			regex input_register("[Rr][Ee][Gg][Ii][Ss][Tt][Ee]Rr]");
+
 			string messageToSend;
 			if (!logged) {
 				do {
-					cout << "> ";
+					cout << ">> ";
 					getline(cin, msg); // nhập tin nhắn muốn gửi
 					messageToSend = msg;
 				} while (msg == "");
 				hexgate.Sending(messageToSend); // gửi gói tin cho server
 
-				int pos = messageToSend.find_first_of(' ');
-				msg = messageToSend.substr(0, pos);
+				int SpaceIndex = msg.find_first_of(" ");
+				if (SpaceIndex != -1)
+					msg = msg.substr(0, SpaceIndex);
 
-				if (regex_match(msg,input_logout)) {
+				if (msg == "Logout" || msg == "logout") {
 					cout << "See you next time.\n";
 					break;
 				}
-				else if (regex_match(msg,input_login)) {
+				else if (msg == "Login" || msg == "login") {
 					LoginHandle(hexgate, messageToSend);
 					logged = true;
 				}
-				else if (regex_match(msg,input_register))
+				else if (msg == "Register" || msg == "register")
 					RegisterHandle(hexgate, messageToSend);
+				
 			}
 			else {
 				// da login
-				Sleep(50);
-				cout << "### Welcome to BattleShip Online! ###\n";
-				// làm các hàm check user sau khi login tại đây.
-				cout << "> What do you want to do?" << endl;
-				cout << "> 1. Check the status of other players: check_user [-option] [username]" << endl;
-				cout << "> 2. Change your personal information: setup_info [-option] [element]" << endl;
-				cout << "> NOTE: If you need help, just type: /help [funciton]" << endl << endl;
-				string option;
-				int flag = 0;
+				cout << "da login thanh cong.\n";
+
 				do {
+					cout << ">> ";
+					getline(cin, msg); // nhập tin nhắn muốn gửi
+					messageToSend = msg;
+				} while (msg == "");
+				hexgate.Sending(messageToSend); // gửi gói tin cho server
 
-					do {
-						if (get_option(option) == 0 && flag != 0) 
-							cout << "> Function unrecognized! Please re-enter";	
-						cout << "> Your choice: ";
-						getline(cin, option);
-						cout << endl;
-						flag++;
-					} while (get_option(option) == 0);
+				int SpaceIndex = msg.find_first_of(" ");
+				if (SpaceIndex != -1)
+					msg = msg.substr(0, SpaceIndex);
 
-					switch (get_option(option))
+				if (msg == "change_password")
+					ChangePasswordHandle(hexgate, messageToSend);
+				else if (msg == "start_game")
+				{
+					int flag = 0;
+					CreateRoomHandle(hexgate, messageToSend, flag);
+					while (flag == 1)
 					{
-					case 1:
-						hexgate.Sending(option);
-						break;
-					case 2:
-						hexgate.Sending(option);
-						break;
-					case 3:
-						show_help_checkuser();
-						break;
-					case 4:
-						show_help_setup();
-						break;
-					case -1:
-						cout << "Logging out! See you next time.";
-						break;
-					default:
-						break;
+						do {
+							cout << ">> ";
+							getline(cin, msg); // nhập tin nhắn muốn gửi
+							messageToSend = msg;
+						} while (msg == "");
+						hexgate.Sending(messageToSend);
+
+						vector<vector<char>> map, stat;
+
+						int SpaceIndex = msg.find_first_of(" ");
+						if (SpaceIndex != -1)
+							msg = msg.substr(0, SpaceIndex);
+						if (msg == "upload_file")
+						{
+							UploadFileHandle(hexgate, messageToSend, map, stat);
+							PrintPlayBoard(stat, map);
+							string msg = hexgate.Receive();
+							string msgtosend;
+							if (msg == "Game begin")
+							{
+								do
+								{
+									cout << ">> ";
+									getline(cin, msgtosend);
+									hexgate.Sending(msgtosend);
+								} while (true);
+							}
+						}
+						
 					}
-					flag = 0;
-				} while (true);
+				}
 			}
 
 			Sleep(5); // hàm này dùng để dồng bộ giữa thời gian chạy giữa client và server để tránh lệch 
 		}
 	}
+
+
 	return 0;
 }
+	
