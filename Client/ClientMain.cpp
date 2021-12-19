c0700be8a071f2c445a87b806592eeaf76d2b79c
nien11102002 <nguyenduynien11102002@gmail.com>
2021-12-19 17:57:53 +0700

    Update ClientMain.cpp

--- Client/ClientMain.cpp
+++ Client/ClientMain.cpp
@@ -1,4 +1,4 @@
-﻿#include"MyClient.h"
+#include"MyClient.h"
 
 int main() {
 	Client hexgate;
@@ -15,28 +15,30 @@ int main() {
 			string messageToSend;
 			if (!logged) {
 				do {
-					cout << "> ";
+					cout << ">> ";
 					getline(cin, msg); // nhập tin nhắn muốn gửi
 					messageToSend = msg;
 				} while (msg == "");
 				hexgate.Sending(messageToSend); // gửi gói tin cho server
 
-				int pos = messageToSend.find_first_of(' ');
-				msg = messageToSend.substr(0, pos);
+				int SpaceIndex = msg.find_first_of(" ");
+				if (SpaceIndex != -1)
+					msg = msg.substr(0, SpaceIndex);
 
-				if (msg== "Logout" ||msg == "logout") {
+				if (msg == "Logout" || msg == "logout") {
 					cout << "See you next time.\n";
 					break;
 				}
 				else if (msg == "Login" || msg == "login") {
-					LoginHandle(hexgate, messageToSend, logged);
+					LoginHandle(hexgate, messageToSend,logged);
+					logged = true;
 				}
 				else if (msg == "Register" || msg == "register")
 					RegisterHandle(hexgate, messageToSend);
 			}
 			else {
 				// da login
-				Sleep(50);
+				/*Sleep(50);
 				cout << "### Welcome to BattleShip Online! ###\n";
 				// làm các hàm check user sau khi login tại đây.
 				cout << "> What do you want to do?" << endl;
@@ -46,7 +48,9 @@ int main() {
 				string option;
 				int flag = 0;
 				bool running = true;
-				do {
+
+				do 
+				{
 					Sleep(100);
 					string var = hexgate.Receive();
 					if (var != "none") {
@@ -84,20 +88,74 @@ int main() {
 					case 4:
 						show_help_setup();
 						break;
-					case -1:{
-						cout << "Logging out! See you next time."; 
+					case -1: {
+						cout << "Logging out! See you next time.";
 						exit(0);
-						}
-						
+					}
+
 					default:
 						break;
 					}
 					flag = 0;
-				} while (running);
+				} while (running);*/
+				
+				/// //////////
+				do {
+					cout << ">> ";
+					getline(cin, msg);
+					messageToSend = msg;
+				} while (msg == "");
+				hexgate.Sending(messageToSend);
+				
+				int SpaceIndex = msg.find_first_of(" ");
+				if (SpaceIndex != -1)
+					msg = msg.substr(0, SpaceIndex);
+
+				if (msg == "change_password")
+					ChangePasswordHandle(hexgate, messageToSend);
+				else if (msg == "start_game")
+				{
+					int flag = 0; int turn = 0;
+					CreateRoomHandle(hexgate, messageToSend, flag);
+					while (flag == 1)
+					{
+						do {
+							cout << ">> ";
+							getline(cin, msg);
+							messageToSend = msg;
+						} while (msg == "");
+						hexgate.Sending(messageToSend);
+
+						vector<vector<char>> map, stat;
+						int SpaceIndex = msg.find_first_of(" ");
+						if (SpaceIndex != -1)
+							msg = msg.substr(0, SpaceIndex);
+						if (msg == "upload_file")
+						{
+							UploadFileHandle(hexgate, messageToSend, map, stat);
+							string msg = hexgate.Receive();
+							string msgtosend;
+							if (msg == "Your turn")
+							{
+								cout << "Your turn" << endl;
+								Attack(hexgate, messageToSend, 0, map, stat);
+							}
+							else if (msg == "Next turn")
+							{
+								cout << "Next turn" << endl;
+								Attack(hexgate, messageToSend, 1, map, stat);
+							}
+						}
+						
+					}
+				}
 			}
 
 			Sleep(5); // hàm này dùng để dồng bộ giữa thời gian chạy giữa client và server để tránh lệch 
 		}
 	}
+
+
 	return 0;
 }
+	
