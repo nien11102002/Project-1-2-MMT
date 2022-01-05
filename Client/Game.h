42b23b05d5005bfcc4b7958222d2dcc25d428f70
Nguyen Manh Hung <81510821+Hung369@users.noreply.github.com>
2022-01-05 15:14:30 +0700

    Chỉnh lại Game.h (xóa hàm DFS và count)

--- Client/Game.h
+++ Client/Game.h
@@ -15,8 +15,6 @@ static bool game_over = false;
 
 void ReadMap(ifstream& file, vector<vector<char>>& map1);
 void ReadData(ifstream& file, vector<vector<char>>& stat);
-int count(vector<vector<char>>enermystat);
-void DFS(vector<vector<char>>enermystat, int i, int j, int n, vector<vector<bool>>& visited);
 void PrintPlayBoard(vector<vector<char>> cloth, vector<vector<char>> map);
 
 #endif
